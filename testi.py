b9dcb5dcfe78e45b31223708efabe6b79bd94660
ClausHofman <46158@edu.raseko.fi>
2022-01-25 10:54:13 +0200

    ensimmäinen harjoitus

--- testi.py
+++ testi.py
@@ -2,11 +2,14 @@
 
 # Määritellään muuttujat ja kysytään arvot käyttäjältä
 
-first_name = input('Mikä on etunimesi? ')
-favourite_place = input('Mikä on suosikkipaikkasi? ')
-nickname = input('Mikä on lempinimesi? ')
-year_of_birth = input('Minä vuonna olet syntynyt? ')
+# first_name = input('Mikä on etunimesi? ')
+# favourite_place = input('Mikä on suosikkipaikkasi? ')
+# nickname = input('Mikä on lempinimesi? ')
+# year_of_birth = input('Minä vuonna olet syntynyt? ')
 
 # Tulostetaan tiedot ruudulle
-print(first_name, 'olet syntynyt'), year_of_birth)
+# print(first_name, 'olet syntynyt'), year_of_birth)
 
+# Luodaan lista viikonpäivistä, ei välttämättä tarvitse olla välilyöntejä
+viikonpaivat = ['maanantai', 'tiistai', 'keskiviikko', 'torstai', 'perjantai', 'lauantai', 'sunnuntai']
+print('Viikon ensimmäinen päivä on', viikonpaivat[0])
